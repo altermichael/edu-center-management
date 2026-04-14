c9e5c2c751a88b4b985ba79af45f9a9f6fb5af88
Alter Michael <altermichael2007@gmail.com>
2026-04-14 20:45:58 +0400

    html and core models was mini fixed

--- core/models.py
+++ core/models.py
@@ -44,7 +44,7 @@ class Student(models.Model):
     address = models.TextField(blank=True, null=True)
     status = models.CharField(max_length=20, choices=StatusChoices.choices, default=StatusChoices.ACTIVE)
     branch = models.ForeignKey(Branch, on_delete=models.RESTRICT, related_name='students')
-    parent = models.ForeignKey(Parent, on_delete=models.RESTRICT, related_name='children')
+    parent = models.ForeignKey(Parent, on_delete=models.RESTRICT, related_name='children', null=True, blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
