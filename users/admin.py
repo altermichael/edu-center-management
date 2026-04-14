0e5d0df7c08ad7e7a7531837d2a6a568791ee18b
yakov alter <alteryasha1710@gmail.com>
2026-04-14 22:56:08 +0400

    Fixed password hashing for CustomUser in Django admin

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        if obj.password and not obj.password.startswith('pbkdf2_'): #шифруємо звич тектс
+            obj.set_password(obj.password)
+        super().save_model(request, obj, form, change)
+
+admin.site.register(CustomUser, CustomUserAdmin)
